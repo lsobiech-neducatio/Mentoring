35d837d93e06ffedd76785a3443f875eb241114a
lsobiech-neducatio <lsobiech@n-educatio.pl>
2018-04-16 09:14:04 +0200

    added todo list

---
+++ todo list/main.js
@@ -0,0 +1,11 @@
+
+// click on the "add" button
+document.getElementById("add").addEventListener("click", function() {
+  const newTodo = document.getElementById("newTodo").value;
+
+  // if input field is not empty, add the new todo to the list
+  if (newTodo) {
+    
+  }
+
+});
